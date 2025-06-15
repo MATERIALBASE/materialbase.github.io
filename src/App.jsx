1803037fd6d2f7b46e039f60a4f25f84f8ab01a9
N.SRIMANNARAYANA REDDY <101311540+SRIMAN7382@users.noreply.github.com>
2025-06-15 11:25:53 +0530

    Revert "Google Authentication Login for SASTRA College"
    This reverts commit 0b88b007d9ab6f82a83109b748c16c2282380e56.

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,16 @@ import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { motion, AnimatePresence } from 'framer-motion'
 import { ThemeProvider } from './contexts/ThemeContext'
-import { AuthProvider } from './contexts/AuthContext'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Semesters from './pages/Semesters'
 import SemesterDetail from './pages/SemesterDetail'
 import Papers from './pages/Papers'
 import Calendar from './pages/Calendar'
-import Login from './pages/Login'
 import Footer from './components/Footer'
 import LoadingScreen from './components/LoadingScreen'
 import TermsModal from './components/TermsModal'
 import CalendarNotification from './components/CalendarNotification'
-import ProtectedRoute from './components/ProtectedRoute'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -48,44 +45,21 @@ function App() {
 
   return (
     <ThemeProvider>
-      <AuthProvider>
-        <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
-          <Navbar />
-          <AnimatePresence mode="wait">
-            <Routes>
-              <Route path="/login" element={<Login />} />
-              <Route path="/" element={
-                <ProtectedRoute>
-                  <Home />
-                </ProtectedRoute>
-              } />
-              <Route path="/semesters" element={
-                <ProtectedRoute>
-                  <Semesters />
-                </ProtectedRoute>
-              } />
-              <Route path="/semester/:id" element={
-                <ProtectedRoute>
-                  <SemesterDetail />
-                </ProtectedRoute>
-              } />
-              <Route path="/papers" element={
-                <ProtectedRoute>
-                  <Papers />
-                </ProtectedRoute>
-              } />
-              <Route path="/calendar" element={
-                <ProtectedRoute>
-                  <Calendar />
-                </ProtectedRoute>
-              } />
-            </Routes>
-          </AnimatePresence>
-          <Footer />
-          <TermsModal isOpen={showTerms} onAccept={handleAcceptTerms} />
-          <CalendarNotification />
-        </div>
-      </AuthProvider>
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+        <Navbar />
+        <AnimatePresence mode="wait">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/semesters" element={<Semesters />} />
+            <Route path="/semester/:id" element={<SemesterDetail />} />
+            <Route path="/papers" element={<Papers />} />
+            <Route path="/calendar" element={<Calendar />} />
+          </Routes>
+        </AnimatePresence>
+        <Footer />
+        <TermsModal isOpen={showTerms} onAccept={handleAcceptTerms} />
+        <CalendarNotification />
+      </div>
     </ThemeProvider>
   )
 }
